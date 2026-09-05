c4833592bfeeb63a4723ce47a3beb1e0a6c7dee2
elshamathew23-glitch <elshamathew23@gmail.com>
2026-09-05 21:36:00 +0530

    Salary coding

---
+++ src/main/java/Coding/SecondLargestSalary.java
@@ -0,0 +1,19 @@
+package Coding;
+
+import java.util.Arrays;
+import java.util.Optional;
+import java.util.stream.Stream;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Stream;
+
+public class SecondLargestSalary {
+    public static void main(String[] args) {
+        List<Integer> salary = Arrays.asList(1000, 5000, 4999, 3000, 3000, 1500);
+        Optional<Integer> res = salary.stream().distinct().sorted(Comparator.reverseOrder()).skip(1).findFirst();
+
+        res.ifPresent(r -> System.out.println(r));
+
+        // salary.stream().distinct().skip(1).sorted(Comparator.reverseOrder()).forEach(System.out::println);
+    }
+}
